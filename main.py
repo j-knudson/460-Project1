6daba5a736207098b02450768d46890c96a25332
j-knudson <knudsonjd@gmail.com>
2021-09-27 09:52:53 -0500

    added check to not step on mountain and testing secondary images

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Toon(pygame.sprite.Sprite):
     def Move(self):
 
         randomDirection = random.randint(0,8) #create a random value to choose a direction
-        randomDistance = random.randint(1,2) #create a reandom value to choose a distance 1 square or 2 squares
+        randomDistance = random.randint(1,2) #create a reandom value to choose a distance 1; square or 2 squares
 
 
         #create a series of if statements to determine 1 or more directions for the toon to move
@@ -76,14 +76,14 @@ class Toon(pygame.sprite.Sprite):
             print(self.name, " hit ", contact.name)
             if contact.carrot:
                 print(contact.name, "had the carrot and it is stolen!")
-                self.carrot = True
+                self.hascarrot = True
             pygame.sprite.Sprite.kill(contact)
         if pygame.sprite.spritecollideany(self, carrot):
             print(self.name, "got the carrot!")
-            self.carrot = True
+            self.hascarrot = True
             pygame.sprite.Sprite.kill(carrot1)
         if pygame.sprite.spritecollideany(self, mountain):
-            if self.carrot:
+            if self.hascarrot:
                 print(self.name, "brought the carrot to the mountain and won!")
                 pygame.quit()
                 sys.exit()
@@ -99,8 +99,11 @@ class Toon(pygame.sprite.Sprite):
             self.rect.move_ip(0, -100)
             distance_moved +=1
             test = pygame.sprite.spritecollideany(self, players)
-            if test and self.avoid:
+            if test and self.avoid:         #create a check to avoid other players if not marvin
                 self.rect.move_ip(0, 100)
+                break
+            if pygame.sprite.spritecollideany(self, mountain) and not self.hascarrot: #check if contact with mountain and avoid if no carrot
+                self.rect.move_ip(0,100)
                 break
         if (self.rect.top < 0):  # check if top of board is reached
             self.rect.top = 0  # if so set position to top of board
@@ -113,6 +116,9 @@ class Toon(pygame.sprite.Sprite):
             if pygame.sprite.spritecollideany(self, players) and self.avoid :
                 self.rect.move_ip(0, -100)
                 break
+            if pygame.sprite.spritecollideany(self, mountain) and not self.hascarrot: #check if contact with mountain and avoid if no carrot
+                self.rect.move_ip(0,-100)
+                break
         if (self.rect.bottom > SCREEN_HEIGHT):  # check if bottom reached
             self.rect.bottom = SCREEN_HEIGHT  # if so set position to bottom
 
@@ -122,6 +128,9 @@ class Toon(pygame.sprite.Sprite):
             self.rect.move_ip(-100, 0)
             distance_moved +=1
             if pygame.sprite.spritecollideany(self, players) and self.avoid:
+                self.rect.move_ip(100, 0)
+                break
+            if pygame.sprite.spritecollideany(self, mountain) and not self.hascarrot: #check if contact with mountain and avoid if no carrot
                 self.rect.move_ip(100, 0)
                 break
         if (self.rect.left < 0):
@@ -135,6 +144,9 @@ class Toon(pygame.sprite.Sprite):
             if pygame.sprite.spritecollideany(self, players) and self.avoid:
                 self.rect.move_ip(-100, 0)
                 break
+            if pygame.sprite.spritecollideany(self, mountain) and not self.hascarrot: #check if contact with mountain and avoid if no carrot
+                self.rect.move_ip(-100, 0)
+                break
         if (self.rect.right > SCREEN_WIDTH):
             self.rect.right = SCREEN_WIDTH
     def Spawn(self):
@@ -144,17 +156,22 @@ class Toon(pygame.sprite.Sprite):
             self.Spawn
 
     def hasCarrot(self):
-        carrot = false
+        self.hascarrot = false
 
 
 class BugsBunny(Toon):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Assets/bugs2.png")
-        self.rect = self.image.get_rect()
         self.name = "Bugs"
-        self.carrot = False
-        self.avoid = True
+        self.hascarrot = False
+        if self.hascarrot:
+            self.image = pygame.image.load("Assets/bugs2.png")
+        else:
+            self.image = pygame.image.load("Assets/bugs2.png")
+        self.rect = self.image.get_rect()
+        self.avoid = True  #does the toon avoid other toons
+
+
 
         # randomly spawn bugs Creates a random int between 0-4 and then centers it to the midpoint of the 100 pixel sized square
         self.Spawn()
@@ -164,10 +181,13 @@ class BugsBunny(Toon):
 class Taz(Toon):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("Assets/taz.png")
-        self.rect = self.image.get_rect()
         self.name ="Taz"
-        self.carrot = False
+        self.hascarrot = False
+        if self.hascarrot == True:
+            self.image = pygame.image.load("Assets/tazcarrot.png")
+        else:
+            self.image = pygame.image.load("Assets/taz.png")
+        self.rect = self.image.get_rect()
         self.avoid = True
 
         self.Spawn()
@@ -180,7 +200,7 @@ class Tweety(Toon):
         self.image = pygame.image.load("Assets/tweety.png")
         self.rect = self.image.get_rect()
         self.name = "Tweety"
-        self.carrot = False
+        self.hascarrot = False
         self.avoid = True
 
         self.Spawn()
@@ -193,7 +213,7 @@ class Marvin(Toon):
         self.image = pygame.image.load("Assets/marvin.png")
         self.rect = self.image.get_rect()
         self.name = "Marvin"
-        self.carrot = False
+        self.hascarrot = False
         self.avoid = False
         self.Spawn()
 
@@ -225,10 +245,10 @@ all_sprites = pygame.sprite.Group()
 
 
 #creating sprites
-bugsbunny1 = BugsBunny()
+#bugsbunny1 = BugsBunny()
 taz1 = Taz()
-tweety1 = Tweety()
-marvin1 = Marvin()
+#tweety1 = Tweety()
+#marvin1 = Marvin()
 carrot1 = Carrot()
 mountain1 = Mountain()
 
@@ -236,17 +256,17 @@ mountain1 = Mountain()
 
 #players: toons that have turns
 players = pygame.sprite.Group()
-players.add(bugsbunny1)
+#players.add(bugsbunny1)
 players.add(taz1)
-players.add(tweety1)
-players.add(marvin1)
+#players.add(tweety1)
+#players.add(marvin1)
 
 #all sprites: all created toons
-all_sprites.add(bugsbunny1)
+#all_sprites.add(bugsbunny1)
 all_sprites.add(taz1)
-all_sprites.add(tweety1)
-all_sprites.add(marvin1)
-# all_sprites.add(carrot1)
+#all_sprites.add(tweety1)
+#all_sprites.add(marvin1)
+all_sprites.add(carrot1)
 all_sprites.add(mountain1)
 
 #carrot group:
@@ -277,7 +297,7 @@ while True:
         players.remove(player)
         player.Move()
         players.add(player)
-        pygame.time.delay(10)
+        pygame.time.delay(500)
 
     pygame.display.update()
     FramePerSec.tick(FPS)
